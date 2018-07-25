08b9c70295f6283370eba0dbf0fc0d31fd292f54
Oleksandr Vosmushko <sasha.vosmushko@gmail.com>
2018-07-25 17:49:07 +0300

    fix soon

---
+++ s2geometrytest/Index.cs
@@ -0,0 +1,100 @@
+﻿using BTree;
+using Google.Common.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace s2geometrytest
+{
+    class UserList
+    {
+        public S2CellId s2CellId;
+
+        public List<Guid> list; 
+    }
+
+    class Index
+    {
+        public static S2Point pointFromLatLng(double lat, double lon)
+        {
+            var phi = ConvertToRadians(lat);
+            var theta = ConvertToRadians(lon);
+            var cosPhi = Math.Cos(phi);
+            return new S2Point(Math.Cos(theta) * cosPhi, Math.Sin(theta) * cosPhi, Math.Sin(phi));
+        }
+        public static double ConvertToRadians(double angle)
+        {
+            return (Math.PI / 180) * angle;
+        }
+
+        const double EarthRadiusM = 6371010.0;
+
+        private int _level;
+
+        BTree<S2CellId, List<Guid>> tree;
+
+        public Index ( int level = 30 )
+        {
+            tree = new BTree<S2CellId, List<Guid>>(35);
+            _level = level;
+        }
+
+        public void AddUser(Guid uid, double lon, double lat)
+        {
+            var lonLat = S2LatLng.FromDegrees(lat, lon);
+
+            var cellId = S2CellId.FromLatLng(lonLat);
+
+            var cellIdStorageLevel = cellId.ParentForLevel(_level);
+
+            var userList = new UserList { s2CellId = cellIdStorageLevel, list = new List<Guid>() };
+
+            var item = tree.Search(userList.s2CellId);
+
+            if (item != null)
+            {
+                userList = new UserList { s2CellId = item.Key, list  = item.Pointer};
+
+                tree.Delete(userList.s2CellId);
+            }
+
+            if (userList.list == null)
+            {
+                userList.list = new List<Guid>();
+                
+            }
+            userList.list.Add(uid);
+
+            tree.Insert(userList.s2CellId, userList.list);
+        }
+
+        public List<Guid> Search (double lon, double lat, int radius) 
+        {
+            var latlng = S2LatLng.FromDegrees(lat, lon);
+
+            var centerPoint = pointFromLatLng(lat,lon);
+
+            var centerAngle = ((double)radius) / EarthRadiusM;
+
+            var cap = S2Cap.FromAxisAngle(centerPoint, S1Angle.FromRadians(centerAngle));
+
+            var regionCoverer = new S2RegionCoverer() { MinLevel = 15, MaxLevel = 5, MaxCells = 1000};
+
+            var covering = regionCoverer.GetCovering(cap);
+
+            var res = new List<Guid>();
+
+            foreach (var u in covering)
+            {
+                var sell = new S2CellId(u.Id);
+                var item = tree.Search(sell);
+                if (item != null)
+                {
+                    res.AddRange(item.Pointer);
+                }
+            }
+            return res;
+        }
+
+    }
+}
---
+++ s2geometrytest/Program.cs
@@ -0,0 +1,56 @@
+﻿using Google.Common.Geometry;
+using System;
+
+namespace s2geometrytest
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+
+            //    var point = new S2Point();
+
+            //var cell = new S2CellId();
+
+
+            //var point = Index.pointFromLatLng(47.678956162876, -122.12871664749933);
+
+            //var latlon = new S2LatLng(point);
+
+            //var lat = S2LatLng.FromDegrees(47.678956162876, -122.12871664749933);
+
+            //S2CellId id = S2CellId.FromLatLng(lat);
+
+            //var lon = id.ToLatLng();
+
+
+            var i = new Index(13);
+
+            i.AddUser(Guid.NewGuid(), 14.1313, 14.1313);
+
+            i.AddUser(Guid.NewGuid(), 14.1314, 14.1314);
+
+            i.AddUser(Guid.NewGuid(), 14.1311, 14.1311);
+
+            i.AddUser(Guid.NewGuid(), 14.2313, 14.2313);
+
+            i.AddUser(Guid.NewGuid(), 14.0313, 14.0313);
+
+            TestSearch(i);
+
+            Console.WriteLine();
+        }
+
+        public static void TestSearch(Index i)
+        {
+            var found = i.Search(14.1313, 14.1313, 20000);
+
+            return;
+        }
+
+     
+
+    }
+}
